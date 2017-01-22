3e4be1bbe11ca54e80ce26c328170fd0d16307f3
Jonathan Tefera <jonathantefera@outlook.com>
2017-01-21 22:40:05 -0800

    Removed DedupePlugin from webpack that was causing some errors

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,6 @@ module.exports = [{
                 'NODE_ENV': JSON.stringify('production'),
                 },
             }),
-            new webpack.optimize.DedupePlugin(),
             new webpack.optimize.OccurenceOrderPlugin(true),
         ],
     }
